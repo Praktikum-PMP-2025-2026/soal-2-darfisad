6b0c8f714dd50e38aa85a2fed22da37163d289af
darfisad <darfisad@gmail.com>
2026-04-15 08:43:44 +0700

    Implement Collatz steps calculation in soal2.c
    Added functions for calculating steps to reach 1 using the Collatz conjecture.

--- soal2.c
+++ soal2.c
@@ -1,5 +1,35 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 1 - OVERVIEW OF C LANGUAGE
+ *   Hari dan Tanggal    : Rabu, 15 April 2026
+ *   Nama (NIM)          : Darfis Ahmad Dostriano (13224096)
+ *   Nama File           : Batu_Stabil.c
+ *   Deskripsi           : 
+ *
+ */
 
-int main(void) {
-    return 0;
+ #include <stdio.h>
+ #include <string.h>
+
+int langkah(int n) {
+    if (n==1) return 0;
+    if (n%2==0) return 1 + langkah(n/2);
+    else return 1 + langkah(3*n+1);
 }
+void transformasi(int n, int steps) {
+    if (n == 1) {
+        printf("Jumlah langkah untuk mencapai 1: %d\n", steps);
+        return;
+    }
+    if (n % 2 == 0) {
+        transformasi(n / 2, steps + 1);
+    } else {
+        transformasi(3 * n + 1, steps + 1);
+    }
+}
+int main () {
+    int n;
+    printf("Masukan angka: ");
+    scanf("%d", &n);
+    printf("LANGKAH %d\n", langkah(n));
+    return 0;
+ }
