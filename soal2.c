f45c645389bda5067a9a61a053fc8074bceb06b5
darfisad <darfisad@gmail.com>
2026-04-15 08:58:57 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -15,21 +15,13 @@ int langkah(int n) {
     if (n%2==0) return 1 + langkah(n/2);
     else return 1 + langkah(3*n+1);
 }
-void transformasi(int n, int steps) {
-    if (n == 1) {
-        printf("Jumlah langkah untuk mencapai 1: %d\n", steps);
-        return;
-    }
-    if (n % 2 == 0) {
-        transformasi(n / 2, steps + 1);
-    } else {
-        transformasi(3 * n + 1, steps + 1);
-    }
-}
 int main () {
-    int n;
-    printf("Masukan angka: ");
-    scanf("%d", &n);
+    int T, i, n;
+    scanf("%d", &T);
+    for (i = 0; i < T; i++) {
+        scanf("%d", &n);
+        // transformasi(n, 0);
     printf("LANGKAH %d\n", langkah(n));
+    }
     return 0;
- }
+}
